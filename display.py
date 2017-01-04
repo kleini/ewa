4438ee64d03d82dfbca83efa925db6fad44d1141
Marcus Klein <marcus.klein@open-xchange.com>
2017-01-04 19:36:33 +0100

    added second page for selecting the force

--- display.py
+++ display.py
@@ -1,18 +1,31 @@
 import sys
 
 from kivy.app import App
-from kivy.properties import ObjectProperty
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
+from kivy.properties import ObjectProperty
+from kivy.uix.pagelayout import PageLayout
 
 
-class Display(BoxLayout):
-    gauge = ObjectProperty(None)
-    slider = ObjectProperty(None)
+class Display(PageLayout):
+    torque_gauge = ObjectProperty(None)
+    force_select_button = ObjectProperty(None)
+    max_force_select = ObjectProperty(None)
+    force_select_display = ObjectProperty(None)
 
-    def set_gauge(self, value):
-        self.gauge.value = value
+    def set_torque(self, value):
+        self.torque_gauge.value = value
+
+    def biplace(self, value):
+        if value:
+            self.max_force_select.disabled = True
+            self.select_force(130)
+        else:
+            self.max_force_select.disabled = False
+            self.select_force(self.max_force_select.value)
+
+    def select_force(self, value):
+        self.force_select_display.text = str(value) + " kg"
+        self.force_select_button.text = str(value) + " kg"
 
 
 class DisplayApp(App):
