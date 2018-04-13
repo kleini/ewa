955dc67fcfc05425b95544c792413fb3f91eb52c
Marcus Klein <marcus.klein@open-xchange.com>
2018-04-13 12:21:37 +0200

    started with class for throttle

---
+++ throttle.py
@@ -0,0 +1,14 @@
+import smbus
+
+
+class Throttle:
+    def __init__(self):
+        self.smbus = SMBus()
+
+
+    def write:
+        smbus.write_i2c_block(address, reg, data)
+
+
+    def read:
+        smbus.read_byte_data(address, reg)
